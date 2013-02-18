1bc65f15733c37a5a0169e47349ae4fa5837e14d
tony <zsqucj@sina.com>
2013-02-18 10:51:16 +0800

    Initial Commit

---
+++ qwe123/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  qwe123
+//
+//  Created by tony J on 13-2-18.
+//  Copyright (c) 2013年 曲超杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
